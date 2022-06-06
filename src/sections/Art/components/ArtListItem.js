1a93b9a27a3f2d664ece1eff5dd5c3f17d01d1c3
Nathan King <nathanking3590@gmail.com>
2022-06-06 17:53:35 +0100

    add art solution

---
+++ src/sections/Art/components/ArtListItem.js
@@ -0,0 +1,18 @@
+function ArtListItem({ title, artist, imageUrl, subjects }) {
+  return (
+    <li>
+        <div className="frame">
+            <img src={imageUrl} />
+        </div>
+
+        <h3>{title}</h3>
+        <p>Artist: {artist}</p>
+        <h4>Artistic Subjects:</h4>
+        <ul>
+            {subjects && subjects.map((subject, index) => <li key={index}>{subject}</li>)}
+        </ul>
+    </li>
+  )
+}
+
+export default ArtListItem
--- src/sections/Art/index.js
+++ src/sections/Art/index.js
@@ -1,10 +1,42 @@
 import { useEffect, useState } from "react"
+import ArtList from "./components/ArtList"
+
+const API_URL = 'https://api.artic.edu/api/v1/artworks?fields=title,artist_title,subject_titles,image_id'
+const IMG_RESOURCE = '/full/843,/0/default.jpg'
 
 function ArtsSection() {
+  const [artworks, setArtworks] = useState([])
+
+  const getArtworks = async () => {
+      const response = await fetch(API_URL)
+      const json = await response.json()
+
+      if (!json.data) {
+          return
+      }
+
+      const iiifUrl = json.config.iiif_url.replace('\\', '')
+
+      const mappedArtworks = json.data.map(art => {
+          return {
+              ...art,
+              image_url: `${iiifUrl}/${art.image_id}${IMG_RESOURCE}`
+          }
+      })
+
+      setArtworks(mappedArtworks)
+  }
+
+  useEffect(() => {
+    getArtworks()
+  }, [])
+  
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+          <ArtList artworks={artworks} />
+      </div>
     </section>
   )
 }
