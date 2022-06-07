42fcbe8a8ef34aff605ce56bee614194eeefe1eb
Nathan King <nathanking3590@gmail.com>
2022-06-07 10:03:41 +0100

    add extension exemplar

---
+++ src/sections/Advice/components/FavouriteSlipsList.js
@@ -0,0 +1,9 @@
+function FavouriteSlipsList({ favourites }) {
+    return (
+        <ul>
+            {favourites.map((advice, index) => <li key={index}>{advice}</li>)}
+        </ul>
+    )
+}
+
+export default FavouriteSlipsList
--- src/sections/Advice/index.js
+++ src/sections/Advice/index.js
@@ -1,11 +1,47 @@
 import { useEffect, useState } from "react"
+import AdviceSlip from "./components/AdviceSlip"
+import FavouriteSlipsList from "./components/FavouriteSlipsList"
+
+const API_URL = 'https://api.adviceslip.com/advice'
 
 function AdviceSection() {
+  const [advice, setAdvice] = useState('')
+  const [favourites, setFavourites] = useState([])
+
+  const addToFavourites = () => {
+      const exists = favourites.find(el => el === advice)
+
+      if (!exists) {
+        setFavourites([...favourites, advice])
+      }
+  }
+
+  const getAdvice = async () => {
+      const response = await fetch(API_URL)
+      const json = await response.json()
+
+      console.log(json)
+
+      setAdvice(json.slip.advice)
+  }
+
+  useEffect(() => {
+    getAdvice()
+  }, [])
+
   return (
     <section>
       <h2>Advice Section</h2>
-      <section className="adivce-slip"></section>
-      <section className="favourtite-slips-list"></section>
+      <section className="adivce-slip">
+        <AdviceSlip advice={advice} />
+        <button onClick={getAdvice}>Get More Advice</button>
+        <button onClick={addToFavourites}>Save to Favourties</button>
+      </section>
+
+      <section className="favourtite-slips-list">
+        <h3>Favourite Advice Slips</h3>
+        <FavouriteSlipsList favourites={favourites} />
+      </section>
     </section>
   )
 }
